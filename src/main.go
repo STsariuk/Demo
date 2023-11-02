c8e573db447a14e704d5bf6378acde444915818b
STsariuk <sviatoslav.tsariuk@gmail.com>
2023-11-02 15:44:47 +0200

    change main file, autosave

--- src/main.go
+++ src/main.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func main()  {
-	
-	http.ListenAndServe(":8080",http.FileServer(http.Dir("")))
+func main() {
+
+	http.ListenAndServe(":8080", http.FileServer(http.Dir("./html")))
 }
